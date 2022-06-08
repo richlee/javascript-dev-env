59efb1da05f17885bf3038e8248c8dec044207d5
rich <richalee@pm.me>
2022-06-08 17:10:07 +0100

    http requests

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // hard coded for simplicity
+  res.json([
+    {"id": 1, "firstName":"Bob","lastname":"Smith","email":"bob@gmail.com"},
+    {"id": 2, "firstName":"Tammy","lastname":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3, "firstName":"Tina","lastname":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
