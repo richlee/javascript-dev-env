38981444b9a3bacdb34a6b807fd03f4f1bb35276
rich <richalee@pm.me>
2022-06-08 11:16:27 +0100

    bundling with webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,19 @@
+import path from "path";
+
+export default {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "src"), // in dev mode the bundle is created in memory, so no 'dist' folder is required
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  plugins: [],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }, // so we can *import* CSS just as we do JS
+    ],
+  },
+};
